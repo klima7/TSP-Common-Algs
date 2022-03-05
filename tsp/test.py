a2bd46ffd8785d04850c6f2b322c0aa3e021cffe
Łukasz Klimkiewicz <ukasz.klimkiewicz@gmail.com>
2022-03-05 10:22:25 +0100

    Comments added

--- tsp/test.py
+++ tsp/test.py
@@ -36,13 +36,18 @@ def test(cities, start_city, connections_drop, symmetric, search_method, seed=No
     display_test_header(connections_drop, symmetric, search_method)
     start_time = time.time()
 
+    # 2. Represent the created map as a weighted, directed graph
     graph = generate_graph(cities, connections_drop=connections_drop, symmetric=symmetric, seed=seed)
 
+    # Make sure whether generated graph is connected
+    # This is necessary condition, but not sufficient
+    # It may be still impossible to visit every city only once
     connected = check_connected(graph)
     if not connected:
         display_test_result(connected=False)
         return
 
+    # 3. Search graph
     result = search_method(graph, start_city)
 
     end_time = time.time()
@@ -54,6 +59,7 @@ if __name__ == '__main__':
     cities_count = 8
     start_city = 0
 
+    # 1. Create a set of cities
     cities = City.generate(count=cities_count, x_range=(-100, 100), y_range=(-100, 100), z_range=(0, 50), seed=seed)
     display_cities(cities)
 
