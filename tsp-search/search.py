c013377d0806186b718e3ecc61b7ff7d78cfb20e
klima7 <ukasz.klimkiewicz@gmail.com>
2022-03-10 18:06:01 +0100

    Nearest insertion cleanup

--- tsp-search/search.py
+++ tsp-search/search.py
@@ -34,84 +34,6 @@ def _generic_exhaustive_search(graph, start, pop_position):
     return optimal_path, optimal_cost
 
 
-def nearest_neighbor(graph, start):
-    cities_count = graph.shape[0]
-    state = (0, [start])
-
-    for i in range(cities_count):
-        next_states = _get_next_states(graph, state)
-
-        if not next_states:
-            return None
-
-        best_next_state = sorted(next_states)[0]
-        state = best_next_state
-
-    cost, path = state
-    return path, cost
-
-
-def nearest_insertion(graph, start):
-    cities_count = graph.shape[0]
-    path = [start, start]
-    # print(graph)
-    # print(path, _calc_path_cost(graph, path))
-
-    for i in range(cities_count - 1):
-
-        nearest_city = _get_city_nearest_to_path(graph, path)
-        # print('Best city:', nearest_city)
-
-        if not nearest_city:
-            return
-
-        path = _insert_city_to_path(graph, path, nearest_city)
-
-        if path is None:
-            return
-
-    cost = _calc_path_cost(graph, path)
-    return path, cost
-
-
-def _get_city_nearest_to_path(graph, path):
-    cities_count = graph.shape[0]
-    not_visited_cities = [i for i in range(cities_count) if i not in path]
-    city_distances = []
-    for target_city in not_visited_cities:
-        minimum_distance = math.inf
-        for start_city in path:
-            distance = graph[start_city, target_city]
-            if distance != -math.inf:
-                minimum_distance = min(minimum_distance, distance)
-        if minimum_distance != math.inf:
-            city_distance = (minimum_distance, target_city)
-            city_distances.append(city_distance)
-    # print('Nearest cities:', sorted(city_distances))
-    nearest_city = sorted(city_distances)[0][1] if city_distances else None
-    return nearest_city
-
-
-def _insert_city_to_path(graph, path, city):
-    possible_paths = [path[:pos] + [city] + path[pos:] for pos in range(1, len(path))]
-    # print(possible_paths)
-    paths_lengths = [(_calc_path_cost(graph, path), path) for path in possible_paths]
-    # print('All insertions:', paths_lengths)
-    possible_paths_lengths = [path_length for path_length in paths_lengths if path_length and path_length[0] != -math.inf]
-    # print('Possible insertions:', possible_paths_lengths)
-    shortest_path = sorted(possible_paths_lengths)[0][1] if possible_paths_lengths else None
-    return shortest_path
-
-
-def _calc_path_cost(graph, path):
-    total_cost = 0
-    for current_city, next_city in zip(path, path[1:]):
-        cost = graph[current_city, next_city]
-        # print(f'cost {current_city}={next_city}: {cost}')
-        total_cost += cost
-    return total_cost
-
-
 def _is_acceptable_state(graph, state):
     cities_count = graph.shape[0]
     path_length = len(state[1])
@@ -155,3 +77,75 @@ def _get_next_states_final_step(graph, state):
 
     new_state = (current_cost + cost, current_path + [first_city])
     return [new_state]
+
+
+def nearest_neighbor(graph, start):
+    cities_count = graph.shape[0]
+    state = (0, [start])
+
+    for i in range(cities_count):
+        next_states = _get_next_states(graph, state)
+
+        if not next_states:
+            return None
+
+        best_next_state = sorted(next_states)[0]
+        state = best_next_state
+
+    cost, path = state
+    return path, cost
+
+
+def nearest_insertion(graph, start):
+    cities_count = graph.shape[0]
+    path = [start, start]
+
+    for i in range(cities_count - 1):
+
+        nearest_city = _find_city_nearest_to_path(graph, path)
+
+        if not nearest_city:
+            return
+
+        path = _get_path_with_inserted_city(graph, path, nearest_city)
+
+        if path is None:
+            return
+
+    cost = _get_path_cost(graph, path)
+    return path, cost
+
+
+def _find_city_nearest_to_path(graph, path):
+    cities_count = graph.shape[0]
+    not_visited_cities = [i for i in range(cities_count) if i not in path]
+    cities_distances = []
+    for target_city in not_visited_cities:
+        minimum_distance = math.inf
+        for start_city in path:
+            distance = graph[start_city, target_city]
+            if distance != -math.inf:
+                minimum_distance = min(minimum_distance, distance)
+        if minimum_distance != math.inf:
+            city_distance = (minimum_distance, target_city)
+            cities_distances.append(city_distance)
+    if not cities_distances:
+        return None
+    nearest_city = sorted(cities_distances)[0][1]
+    return nearest_city
+
+
+def _get_path_with_inserted_city(graph, path, city):
+    all_paths = [path[:pos] + [city] + path[pos:] for pos in range(1, len(path))]
+    lengths_and_paths = [(_get_path_cost(graph, path), path) for path in all_paths]
+    lengths_and_paths = [length_and_path for length_and_path in lengths_and_paths if length_and_path[0] != -math.inf]
+    shortest_path = sorted(lengths_and_paths)[0][1] if lengths_and_paths else None
+    return shortest_path
+
+
+def _get_path_cost(graph, path):
+    total_cost = 0
+    for current_city, next_city in zip(path, path[1:]):
+        cost = graph[current_city, next_city]
+        total_cost += cost
+    return total_cost
